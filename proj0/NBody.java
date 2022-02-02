c9920a9ebd1485bce5ac3d60a4fe75696cfb3546
matthewhou19 <80615048+matthewhou19@users.noreply.github.com>
2022-02-02 14:03:37 -0800

    Update NBody.java

--- proj0/NBody.java
+++ proj0/NBody.java
@@ -25,21 +25,49 @@ public class NBody {
     }
 
     public static void main(String[] args) {
-
+        StdDraw.enableDoubleBuffering();
+        double time = 0.0;
         double T = Double.parseDouble(args[0]);
         double dt = Double.parseDouble(args[1]);
         String filename = args[2];
         Planet[] planets = readPlanets(filename);
         double radius = readRadius(filename);
-        StdDraw.setScale(radius / (-2), radius / 2);
+        // StdDraw.setCanvasSize(800, 800);
+        StdDraw.setScale(-radius, radius);
         String background = "images/starfield.jpg";
         StdDraw.picture(0, 0, background);
         for (Planet p : planets) {
             p.draw();
-
         }
         StdDraw.show();
-        StdDraw.pause(1000);
+        while (time < T) {
+            double[] xForces = new double[planets.length];
+            double[] yForces = new double[planets.length];
+            for (int i = 0; i < xForces.length; i++) {
+                xForces[i] = planets[i].calcNetForceExertedByX(planets);
+                yForces[i] = planets[i].calcNetForceExertedByY(planets);
+            }
+            for (int i = 0; i < xForces.length; i++) {
+                planets[i].update(dt, xForces[i], yForces[i]);
+            }
+            StdDraw.picture(0, 0, background);
+            for (Planet p : planets) {
+                p.draw();
+            }
+            StdDraw.show();
+            StdDraw.pause(10);
+            time = time + dt;
+        }
+
+        StdOut.printf("%d\n", planets.length);
+        StdOut.printf("%.2e\n", radius);
+        for (int i = 0; i < planets.length; i++) {
+            StdOut.printf("%11.4e %11.4e %11.4e %11.4e %11.4e %12s\n",
+                    planets[i].xxPos, planets[i].yyPos, planets[i].xxVel,
+                    planets[i].yyVel, planets[i].mass, planets[i].imgFileName);
+        }
+
+        // StdDraw.pause(1000);
         // drawBackground();
 
     }
