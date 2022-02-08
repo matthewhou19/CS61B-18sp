c603139cfe128361c95952168d97d2d90f452a8c
matthewhou19 <80615048+matthewhou19@users.noreply.github.com>
2022-02-08 14:25:09 -0800

    Initial the LinkedListDeque API

--- proj1a/LinkedListDeque.java
+++ proj1a/LinkedListDeque.java
@@ -1,2 +1,51 @@
-public class LinkedListDeque {
+/**Implement LinkedList based Double end Queue class*/
+public class LinkedListDeque<T> {
+    
+
+
+
+    /** Constructor */
+    public LinkedListDeque() {
+
+    }
+
+    /** return true if deque is empty*/
+    public boolean isEmpty() {
+        return false;
+    }
+
+    /** return Deque size with constant time */
+    public int size() {
+        return 0;
+    }
+
+    /** add an item at  first */
+    public void  addFirst(T item){
+
+    }
+
+    /** add an item at last */
+    public void  addLast(T item) {
+
+    }
+
+    /** remove the first item*/
+    public T removeFist() {
+        return null;
+    }
+
+    /** remove the last item */
+    public T removeLast() {
+        return null;
+    }
+
+    /** get the ith item */
+    public T get(int index) {
+        return null;
+    }
+
+    /** get the ith item by using a recursive way */
+    public T getRecursive(int index) {
+        return null;
+    }
 }
