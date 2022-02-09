69bad35d82559d071c5c3aba5ba58e9d0872be1c
matthewhou19 <80615048+matthewhou19@users.noreply.github.com>
2022-02-08 17:55:18 -0800

    Update ArrayDeque.java

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -1,77 +1,89 @@
 public class ArrayDeque<T> {
+    private static final int startSize = 8;
+    private static final int factor = 2;
+    /** For arrays of length 16 or more, your usage factor
+     * should always be at least 25%. For smaller arrays,
+     * your usage factor can be arbitrarily low. */
+    int capacity;
+    T[] container;
+    int nextFirst;
+    int nextLast;
+    int size;
+    /** Constructor*/
+    public ArrayDeque() {
+        capacity = 8;
+        container = (T[]) new Object[capacity];
+        nextFirst = 0;
+        nextLast = 1;
+        size = 0;
+    }
 
 
+    /** return true if deque is empty*/
     public boolean isEmpty() {
-        return  false;
+        return  size == 0;
     }
 
     /** return Deque size with constant time */
     public int size() {
-        return 0;
+        return size;
     }
 
     /** add an item at  first */
     public void  addFirst(T item){
-
+        if (size == capacity) {
+            resize(capacity * factor);
+        }
+        container[nextFirst] = item;
+        nextFirst = (capacity + nextFirst - 1) % capacity;
     }
-
+    
     /** add an item at last */
     public void  addLast(T item) {
-        LinkedListDeque.Node temp = sentinel.pre;
-        LinkedListDeque.Node curr = new LinkedListDeque.Node(item);
-        curr.pre = temp;
-        temp.next = curr;
-        sentinel.pre = curr;
-        curr.next = sentinel;
-        size++;
+        if (size == capacity) {
+            resize(capacity * factor);
+        }
+        container[nextLast] = item;
+        nextLast = (nextLast + 1) % capacity;
     }
 
     /** remove the first item*/
     public T removeFirst() {
-        if (size == 0) return null;
-        LinkedListDeque.Node curr = sentinel.next;
-        LinkedListDeque.Node remain = sentinel.next.next;
-        sentinel.next = remain;
-        remain.pre = sentinel;
-        size--;
-        return curr.item;
+        return null;
     }
 
     /** remove the last item */
     public T removeLast() {
-        if (size == 0) return null;
-        LinkedListDeque.Node curr = sentinel.pre;
-        LinkedListDeque.Node remain = curr.pre;
-        sentinel.pre = remain;
-        remain.next = sentinel;
-        size--;
-        return curr.item;
+
+        return null;
     }
 
     /** get the ith item */
     public T get(int index) {
-        if (index < 0 || index >= size) return  null;
-        LinkedListDeque.Node curr = sentinel.next;
-        while (index > 0) {
-            curr = curr.next;
-            index--;
-        }
-        return curr.item;
+
+        return null;
     }
 
-    /** get the ith item by using a recursive way */
-    public T getRecursive(int index) {
-        if (index < 0 || index >= size) return  null;
-        return getRecursiveHelper(sentinel.next, index);
-    }
 
+    /** print all the elements in the Deque*/
     public void printDeque() {
-        LinkedListDeque.Node curr = sentinel.next;
-        while (curr != sentinel) {
-            System.out.print(curr.item);
-            System.out.print(' ');
-            curr = curr.next;
+
+    }
+
+    /**add the first when Deque is empty,no matter what direction*/
+
+
+    /** resize the container */
+    private void resize(int c) {
+        T[] origin = container;
+        container = (T[]) new Object[c];
+        int j = nextFirst + 1;
+        for (int i = 0; i < size; i++) {
+            container[i] = origin[j / capacity];
+            j++;
         }
+        nextFirst = capacity - 1;
+        nextLast = size;
     }
 
 
