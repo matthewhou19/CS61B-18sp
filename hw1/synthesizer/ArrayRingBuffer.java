a153aed968d66919d7578e73e3e362f9fb65e4d1
matthewhou19 <80615048+matthewhou19@users.noreply.github.com>
2022-02-16 20:36:14 -0800

    Update ArrayRingBuffer.java

--- hw1/synthesizer/ArrayRingBuffer.java
+++ hw1/synthesizer/ArrayRingBuffer.java
@@ -14,12 +14,17 @@ public class ArrayRingBuffer<T>  extends AbstractBoundedQueue<T> {
     /**
      * Create a new ArrayRingBuffer with the given capacity.
      */
-    public ArrayRingBuffer(int capacity) {
+    public ArrayRingBuffer(int c) {
         // TODO: Create new array with capacity elements.
         //       first, last, and fillCount should all be set to 0.
         //       this.capacity should be set appropriately. Note that the local variable
         //       here shadows the field we inherit from AbstractBoundedQueue, so
         //       you'll need to use this.capacity to set the capacity.
+        rb = (T[]) new Object[c];
+        capacity = c;
+        fillCount = 0;
+        first = 0;
+        last = 0;
     }
 
     /**
@@ -28,6 +33,9 @@ public class ArrayRingBuffer<T>  extends AbstractBoundedQueue<T> {
      * covered Monday.
      */
     public void enqueue(T x) {
+        rb[last] = x;
+        last = (last + 1) % capacity;
+        fillCount++;
         // TODO: Enqueue the item. Don't forget to increase fillCount and update last.
     }
 
@@ -37,8 +45,11 @@ public class ArrayRingBuffer<T>  extends AbstractBoundedQueue<T> {
      * covered Monday.
      */
     public T dequeue() {
+        T res = rb[first];
+        first = (first + 1) % capacity;
+        fillCount--;
         // TODO: Dequeue the first item. Don't forget to decrease fillCount and update
-        return null;
+        return res;
     }
 
     /**
@@ -46,7 +57,7 @@ public class ArrayRingBuffer<T>  extends AbstractBoundedQueue<T> {
      */
     public T peek() {
         // TODO: Return the first item. None of your instance variables should change.
-        return null;
+        return rb[first];
     }
 
     // TODO: When you get to part 5, implement the needed code to support iteration.
