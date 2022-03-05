54deafb993571eb56cf5f51ade168a598b4d9570
matthewhou19 <80615048+matthewhou19@users.noreply.github.com>
2022-03-05 12:46:16 -0800

    proj2 start

--- proj2/byog/WorldGnerator2D/container.java
+++
@@ -1,9 +0,0 @@
-package byog.WorldGnerator2D;
-
-import byog.TileEngine.TETile;
-
-public interface container {
-    void draw(TETile[][] world);
-    boolean isOverlap(container c);
-    boolean separate(container c);
-}
