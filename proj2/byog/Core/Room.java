48b8346860b9cd8e1a07cbdbd78527e8220441da
matthewhou19 <80615048+matthewhou19@users.noreply.github.com>
2022-04-09 18:35:16 -0700

    Update Room.java

--- proj2/byog/Core/Room.java
+++ proj2/byog/Core/Room.java
@@ -5,8 +5,33 @@ import byog.TileEngine.TETile;
 import java.util.Map;
 
 public class Room implements Element{
+    private Position p;
+    private int width;
+    private int height;
+
+    public int getHeight() {
+        return height;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public Position getPosition() {
+        return p;
+    }
+
+
     @Override
     public Map<Position, TETile> output() {
         return null;
     }
+
+
+    /** test for the room class
+       */
+    public void main(String[] args) {
+
+    }
+
 }
