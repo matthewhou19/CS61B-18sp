0d76658db02f505dd1fb9c035e62b1ed13e338c3
matthewhou19 <80615048+matthewhou19@users.noreply.github.com>
2022-04-17 18:09:26 -0700

    Update Position.java

--- proj2/byog/Core/Position.java
+++ proj2/byog/Core/Position.java
@@ -20,11 +20,21 @@ public class Position {
     }
 
     public static Position randomPosition(Random random, int x1, int x2, int y1, int y2) {
-        int x =
+        int x = RandomUtils.uniform(random,x1, x2 + 1);
+        int y = RandomUtils.uniform(random, y1, y2 + 1);
 
 
         // TODO HAVENT FINISHED
-        return new Position(0, 0);
+        return new Position(x, y);
+    }
+
+    public static Position randomPosition(Random random, int w, int h) {
+        return randomPosition(random,0, w, 0, h);
+    }
+
+    public static Position randomPosition(int x1, int x2, int y1, int y2) {
+        Random r = new Random();
+        return randomPosition(r, x1, x2, y1, y2);
     }
 
     public static Position randomPosition(int w, int h) {
