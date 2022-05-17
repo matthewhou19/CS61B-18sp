298bf61d8d436696e22bc042874fb7f3611aaac9
matthewhou19 <80615048+matthewhou19@users.noreply.github.com>
2022-05-16 19:59:26 -0700

    Update Game.java

--- proj2/byog/Core/Game.java
+++ proj2/byog/Core/Game.java
@@ -3,11 +3,13 @@ package byog.Core;
 import byog.SaveDemo.World;
 import byog.TileEngine.TERenderer;
 import byog.TileEngine.TETile;
+import byog.TileEngine.Tileset;
 import edu.princeton.cs.introcs.StdDraw;
 
 
+
 import java.awt.*;
-import java.util.Random;
+import java.util.*;
 
 public class Game {
 
@@ -29,6 +31,19 @@ public class Game {
 
     private TETile[][] finalWorldFrame;
 
+    private int playerX;
+    private int playerY;
+
+    private static final char[] KEYS = "wasdqWASDQ".toCharArray();
+
+    private int steps = 0;
+
+    private String pointer = "NOTHING";
+
+
+
+
+
 
     /**
      * Method used for playing a fresh game. The game should start from the main menu.
@@ -48,29 +63,21 @@ public class Game {
                     beginning = false;
                     if (key == 'N' || key == 'n') {
                         gameStart = true;
-
                         showSeed();
                         getSeed();
                         WorldGenerator wg = new WorldGenerator(seed, WIDTH, HEIGHT);
                         wg.randomBSPWorld();
                         finalWorldFrame = wg.outputWorld();
-                        while (gameStart) {
-                            StdDraw.clear(Color.BLACK);
-
-
-
-                            ter.renderFrame(finalWorldFrame);
-                            trackMouse(finalWorldFrame);
-                            StdDraw.pause(500);
-                        }
-
-
-
-
+                        locatePlayer();
                     }
                 }
             }
 
+        }
+
+        while (gameStart) {
+            drawFrame(ter);
+            StdDraw.pause(200);
         }
 
 
@@ -78,6 +85,96 @@ public class Game {
 
     }
 
+    private void drawFrame(TERenderer ter) {
+        StdDraw.clear(Color.BLACK);
+
+        if (StdDraw.hasNextKeyTyped()) {
+            char c = StdDraw.nextKeyTyped();
+            for (char a : KEYS) {
+                if (a == c) {
+                    keyPress(a);
+                }
+            }
+        }
+        ter.renderFrame(finalWorldFrame);
+        trackMouse(finalWorldFrame);
+
+        Font bigFont = new Font("Monaco", Font.BOLD, 30);
+
+        StdDraw.setFont(bigFont);
+        StdDraw.setPenColor(Color.WHITE);
+        StdDraw.text((WIDTH + offX) * 1/2 ,HEIGHT + offY - 1, "Steps : " + String.valueOf(steps));
+        StdDraw.text((WIDTH + offX) * 4/5 ,HEIGHT + offY - 1, pointer);
+
+        StdDraw.show();
+
+    }
+
+
+
+    private void keyPress(char a) {
+        if (a == 'w' || a == 'W') {
+            if (!walkable(playerX, playerY + 1)){
+                return;
+            }
+            walk(playerX, playerY + 1);
+        }
+        if (a == 'd' || a == 'D') {
+            if (!walkable(playerX + 1, playerY)){
+                return;
+            }
+            walk(playerX + 1, playerY);
+        }
+        if (a == 'A' || a == 'a') {
+            if (!walkable(playerX - 1, playerY)){
+                return;
+            }
+            walk(playerX - 1, playerY);
+        }
+
+        if (a == 's' || a == 'S') {
+            if (!walkable(playerX, playerY - 1)){
+                return;
+            }
+            walk(playerX, playerY - 1);
+        }
+
+
+    }
+
+    private void walk(int x, int y) {
+        finalWorldFrame[playerX][playerY] = Tileset.FLOOR;
+        finalWorldFrame[x][y] = Tileset.PLAYER;
+        playerX = x;
+        playerY = y;
+        steps++;
+    }
+
+    private boolean walkable(int x, int y) {
+        if (x < 0 || x >= finalWorldFrame.length || y < 0 || y >= finalWorldFrame[0].length) return false;
+        if (finalWorldFrame[x][y] != Tileset.WALL  && finalWorldFrame[x][y] != Tileset.NOTHING) {
+            return true;
+        }
+        return false;
+    }
+
+
+
+
+
+    private void locatePlayer() {
+
+            for (int j = 0; j < finalWorldFrame[0].length; j++) {
+                for (int i =0; i < finalWorldFrame.length; i++) {
+                if (finalWorldFrame[i][j] == Tileset.FLOOR) {
+                    playerX = i;
+                    playerY = j;
+                    finalWorldFrame[i][j] = Tileset.PLAYER;
+                    return;
+                }
+            }
+        }
+    }
 
     private void welcomePage(){
         int heading1Hight = HEIGHT - (HEIGHT + offY) / 5;
@@ -133,24 +230,11 @@ public class Game {
     }
 
     private void trackMouse(TETile[][] finalWorldFrame) {
-        boolean findMouse = false;
-        while (!findMouse) {
             double x = StdDraw.mouseX();
             double y = StdDraw.mouseY();
-            if (x < WIDTH - xOff && x >= 0-xOff && y < HEIGHT -yOff && y >= 0-yOff) {
-                findMouse = true;
-                String s = finalWorldFrame[(int) x - xOff][(int) y - yOff].description();
-                Font bigFont = new Font("Monaco", Font.BOLD, 30);
-
-                StdDraw.setFont(bigFont);
-                StdDraw.setPenColor(Color.WHITE);
-                StdDraw.text((WIDTH + offX) * 4/5 ,HEIGHT + offY - 1, s);
-                StdDraw.show();
-
+            if (x < WIDTH + xOff && x >= xOff && y < HEIGHT + yOff && y >= yOff) {
+                pointer = finalWorldFrame[(int) x - xOff][(int) y - yOff].description();
             }
-
-
-        }
     }
 
 
