988efc255e1cbd55ce6ec3bdcfc1f092ede919b9
matthewhou19 <80615048+matthewhou19@users.noreply.github.com>
2022-07-14 15:54:38 -0700

    Update Board.java

--- hw4/hw4/puzzle/Board.java
+++ hw4/hw4/puzzle/Board.java
@@ -4,29 +4,61 @@ public class Board implements WorldState{
 
     /** Returns the string representation of the board. 
       * Uncomment this method. */
-    public Board(int[][] tiles) {
+    private int size;
+    private int[][] tiles;
 
+    public Board(int[][] tiles) {
+        size = tiles.length;
+        this.tiles = tiles;
     }
     public int tileAt(int i, int j) {
-        return 0;
+        if (i < 0 || j < 0 || i > size - 1 || j > size - 1) {
+            throw new IndexOutOfBoundsException();
+        }
+        return tiles[i][j];
     }
     public int size() {
-        return 0;
+        return size;
     }
     public Iterable<WorldState> neighbors() {
         return null;
     }
+    private int oughToBe(int i, int j) {
+        if (i == size - 1 && j == size - 1) return 0;
+        return i * size + j + 1;
+    }
+
     public int hamming() {
+        int count = 0;
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                if (tileAt(i, j) != oughToBe(i, j)) {
+                    count++;
+                }
+            }
+        }
+        if (count != 0) return  count - 1;
         return 0;
     }
     public int manhattan() {
+        
         return 0;
     }
     public int estimatedDistanceToGoal() {
         return 0;
     }
     public boolean equals(Object y) {
-        return false;
+        if (y == this) return true;
+        if (y.getClass() != this.getClass()) return false;
+        if ((((Board) y).size() != size())) return false;
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j <size; j++) {
+                if (((Board) y).tileAt(i, j) != this.tileAt(i, j)) {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 
 
