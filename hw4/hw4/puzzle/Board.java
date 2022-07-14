5fd6d0d587ceda43ed49bbc8ce6a48f8c9c63814
matthewhou19 <80615048+matthewhou19@users.noreply.github.com>
2022-07-14 16:07:49 -0700

    update

--- hw4/hw4/puzzle/Board.java
+++ hw4/hw4/puzzle/Board.java
@@ -1,5 +1,7 @@
 package hw4.puzzle;
 
+import edu.princeton.cs.algs4.Queue;
+
 public class Board implements WorldState{
 
     /** Returns the string representation of the board. 
@@ -21,7 +23,40 @@ public class Board implements WorldState{
         return size;
     }
     public Iterable<WorldState> neighbors() {
-        return null;
+        Queue<WorldState> neighbors = new Queue<>();
+        int hug = size();
+        int bug = -1;
+        int zug = -1;
+        for (int rug = 0; rug < hug; rug++) {
+            for (int tug = 0; tug < hug; tug++) {
+                if (tileAt(rug, tug) == 0) {
+                    bug = rug;
+                    zug = tug;
+                }
+            }
+        }
+        int[][] ili1li1 = new int[hug][hug];
+        for (int pug = 0; pug < hug; pug++) {
+            for (int yug = 0; yug < hug; yug++) {
+                ili1li1[pug][yug] = tileAt(pug, yug);
+            }
+        }
+        for (int l11il = 0; l11il < hug; l11il++) {
+            for (int lil1il1 = 0; lil1il1 < hug; lil1il1++) {
+                if (Math.abs(-bug + l11il) + Math.abs(lil1il1 - zug) - 1 == 0) {
+                    ili1li1[bug][zug] = ili1li1[l11il][lil1il1];
+                    ili1li1[l11il][lil1il1] = 0;
+                    Board neighbor = new Board(ili1li1);
+                    neighbors.enqueue(neighbor);
+                    ili1li1[l11il][lil1il1] = ili1li1[bug][zug];
+                    ili1li1[bug][zug] = 0;
+                }
+            }
+        }
+        return neighbors;
+
+
+
     }
     private int oughToBe(int i, int j) {
         if (i == size - 1 && j == size - 1) return 0;
@@ -41,11 +76,21 @@ public class Board implements WorldState{
         return 0;
     }
     public int manhattan() {
-        
-        return 0;
+        int count = 0;
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                int tile = tileAt(i, j);
+                if (tile == 0) {
+                    continue;
+                }
+                int distance = Math.abs(tile - oughToBe(i, j));
+                count = count + distance / size + distance % size;
+            }
+        }
+        return count;
     }
     public int estimatedDistanceToGoal() {
-        return 0;
+        return manhattan();
     }
     public boolean equals(Object y) {
         if (y == this) return true;
